c1302168ee01712b500c3fd17d376d5db02910c6
gourab8389 <deyg6988@gmail.com>
2024-09-24 19:37:47 +0530

    Enhance drawing functionality in Home component with canvas resizing and improved stroke handling

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,15 +1,29 @@
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 function Home() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDrawing, setIsDrawing] = useState(false);
 
+    useEffect(() => {
+        const canvas = canvasRef.current;
+
+        if(canvas){
+            const ctx = canvas.getContext("2d");
+            if(ctx) {
+                canvas.width = window.innerWidth;
+                canvas.height = window.innerHeight - canvas.offsetTop;
+                ctx.lineCap = "round";
+                ctx.lineWidth = 3;
+            }
+        }
+    }, [])
+
     const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
         const canvas = canvasRef.current;
-        if(canvas){
+        if (canvas) {
             canvas.style.background = "black";
             const ctx = canvas.getContext("2d");
-            if(ctx){
+            if (ctx) {
                 ctx.beginPath();
                 ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
                 setIsDrawing(true);
@@ -21,16 +35,31 @@ function Home() {
         setIsDrawing(false);
     }
 
-  return (
-    <canvas 
-    ref={canvasRef} 
-    id="canvas"
-    className="absolute top-0 left-0 w-full h-full"
-    onMouseDown={startDrawing}
-    onMouseOut={stopDrawing}
-    onMouseUp={stopDrawing}
-    />
-  )
+    const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        if (!isDrawing) {
+            return
+        };
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext("2d");
+            if (ctx) {
+                ctx.strokeStyle = "white";
+                ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+                ctx.stroke();
+            }
+        }
+    };
+
+    return (
+        <canvas
+            ref={canvasRef}
+            id="canvas"
+            className="absolute top-0 left-0 w-full h-full"
+            onMouseDown={startDrawing}
+            onMouseOut={stopDrawing}
+            onMouseUp={stopDrawing}
+        />
+    )
 }
 
 export default Home;
